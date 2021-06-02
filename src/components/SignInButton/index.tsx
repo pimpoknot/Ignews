d43d07ac92185883f482b15213c1337d30c1e1d9
Marcos Maverick <marcosmaverick@Marcoss-MacBook-Pro.local>
2021-06-01 22:26:42 -0300

    chore: faunadb configurado

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,29 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
-
+import { signIn, useSession, signOut } from 'next-auth/client'
 
 export function SignInButton () {
 
-  const isUserLoggedIn = true
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
+  console.log(session)
+
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={()=>signOut()}
     >
       <FaGithub color="#04d301"/>
-      Marcos Maverick
+      {session.user.name}
       <FiX  color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#ebaf17"/>
       Sign in with GitHub
